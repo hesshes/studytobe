496c3d28cf9f3e1aae822a150bdb61c9dc6b1647
taeu <hesshes@naver.com>
2024-07-09 22:38:27 +0900

    test

--- src/com/hesshes/studytobe/dao/ConnectionMaker.java
+++ src/com/hesshes/studytobe/dao/ConnectionMaker.java
@@ -3,6 +3,6 @@ package com.hesshes.studytobe.dao;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public interface ConnectionMaker {
+public interface ConnectionMaker { 
 	public Connection makeConnection() throws ClassNotFoundException, SQLException;
 }
