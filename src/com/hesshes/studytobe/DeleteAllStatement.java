71ff92a0839daed27b99fa09c93bf8ce09b69b22
taeu <hesshes@naver.com>
2024-08-03 12:32:22 +0900

    20240803 bug/

--- src/com/hesshes/studytobe/DeleteAllStatement.java
+++ src/com/hesshes/studytobe/DeleteAllStatement.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 // list 3-9
-public class DeleteAllStatement implements StatementStrategy {
+public class DeleteAllStatement implements StatementStrategy { 
 	public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
 		PreparedStatement ps = c.prepareStatement("delete from users");
 		return ps;
