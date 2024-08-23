828bb38377e43bebbca92eeedda5aac63defda52
hessh <hessh@HOME-DESK-FROM-SUK>
2024-08-23 23:14:07 +0900

    20240823

--- before_code_list/chapter_1/before_code_domain/User_list_1_1.java
+++ before_code_list/chapter_1/before_code_domain/User_list_1_1.java
@@ -3,32 +3,32 @@ package com.hesshes.studytobe.domain;
 // list-??
 public class User {
 
-	String id;
-	String name;
-	String password;
+    String id;
+    String name;
+    String password;
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
