21689024bf90876c48187e27e57863a4ced09f94
taewoo <hesshes@naver.com>
2024-10-09 21:03:40 +0900

    2024-10-09 testcode 에러 흠..

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -6,17 +6,32 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.domain.User;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
 
-    public UserDao(ConnectionMaker connectionMaker) {
+    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
+
+    public UserDao() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(DaoFactory.class);
+        this.connectionMaker = ctx.getBean("connectionMaker", ConnectionMaker.class);
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values ( ?, ?, ? )");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -29,7 +44,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ? ");
         ps.setString(1, id);
